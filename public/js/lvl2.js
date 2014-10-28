6068a4f50abb036d6ff7cf283d2dacb0fe89cf3f
Mikey Badr <mfbadr@gmail.com>
2014-10-28 14:18:34 -0500

    lvl2

---
+++ public/js/lvl2.js
@@ -0,0 +1,16 @@
+(function(){
+  game.state.add('lvl2', {create:create, update:update});
+
+  //variables are declared here
+
+  function create(){
+    //reset score and time
+
+    //create the game world
+    //timers
+  };
+  function update(){
+    //physics collisions declared here
+    //input controls
+  };
+})();
