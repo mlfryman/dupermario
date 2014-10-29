051eb4fc754e9f0c43b005adac21f6df9ebf48fa
Mikey Badr <mfbadr@gmail.com>
2014-10-29 15:30:29 -0500

    goomba var dec to main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,2 @@
 var game = new Phaser.Game(800, 240, Phaser.CANVAS, 'duper-mario');
-var map, layer, player, score, txtScore, time, timer, txtTime, coin, coins, coinSound;
+var map, layer, player, score, txtScore, time, timer, txtTime, coin, coins, coinSound, goombas;
